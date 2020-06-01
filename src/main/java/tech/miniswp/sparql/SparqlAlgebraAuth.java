59dbf4383aad83de3d76b22e51c9cd89642f9a9e
Rafael Espino <rafael.espino@gmail.com>
2020-06-01 08:23:16 +0200

    Updates Algebra

--- src/main/java/tech/miniswp/sparql/SparqlAlgebraAuth.java
+++ src/main/java/tech/miniswp/sparql/SparqlAlgebraAuth.java
@@ -1,9 +1,11 @@
 package tech.miniswp.sparql;
 
+import org.apache.jena.graph.Triple;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.sparql.algebra.*;
 import org.apache.jena.sparql.algebra.op.*;
+import org.apache.jena.sparql.core.BasicPattern;
 import org.apache.jena.sparql.expr.Expr;
 import org.apache.jena.sparql.expr.ExprList;
 
@@ -35,7 +37,24 @@ public class SparqlAlgebraAuth {
                 "    #FILTER NOT EXISTS { <http://simuino.com/$level> <http://simuino.com/hasProtection> ?m . FILTER(?p1 = ?m)}\n" +
                 "}";
 
-        Query query = QueryFactory.create(queryString) ;
+        String queryString1 = "PREFIX sp: <http://www.semanticweb.org/fwxda9/ontologies/2020/1/scaniaperson#>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "\n" +
+                "\n" +
+                "SELECT ?emp ?p ?o ?p1 ?o2 ?dep\n" +
+                "WHERE {\n" +
+                "    \n" +
+                "    {\n" +
+                "    ?emp sp:worksAtDepartment sp:IXCA .\n" +
+                "    ?emp ?p ?o .\n" +
+                "    ?o ?p1 ?o2 .\n" +
+                "}\n" +
+                "    UNION {\n" +
+                "    ?dep rdf:type sp:Department\n" +
+                "}\n" +
+                "}";
+
+        Query query = QueryFactory.create(queryString1) ;
 
         System.out.println("Query: ");
         System.out.println(query);
@@ -45,6 +64,7 @@ public class SparqlAlgebraAuth {
         System.out.println(op);
         OpWalker.walk(op, new AuthOpBGPVisitor());
         OpWalker.walk(op, new AuthOpProjectVisitor());
+        OpWalker.walk(op, new AuthOpUnionVisitor());
         OpWalker.walk(op, new AuthOpFilterVisitor());
     }
 
@@ -54,9 +74,28 @@ public class SparqlAlgebraAuth {
             System.out.println("Visitor BGP: ");
             System.out.println(opBGP);
             System.out.println(opBGP.getName());
-            System.out.println(opBGP.getPattern().get(0).getSubject());
-            System.out.println(opBGP.getPattern().get(0).getPredicate());
-            System.out.println(opBGP.getPattern().get(0).getObject().isVariable());
+            int i = 0;
+            for (Triple pttr : opBGP.getPattern().getList()) {
+                System.out.println("BGP: " + i); i++;
+                System.out.println(pttr.getSubject());
+                System.out.println(pttr.getPredicate());
+                System.out.println(pttr.getObject());
+            }
+        }
+    }
+
+    public static class AuthOpUnionVisitor extends OpVisitorBase {
+        @Override
+        public void visit(OpUnion opUnion) {
+            System.out.println("UNION BGP: ");
+            System.out.println(opUnion);
+            System.out.println(opUnion.getName());
+
+            System.out.println("Union Left: " + opUnion.getLeft());
+            OpWalker.walk(opUnion.getLeft(), new AuthOpBGPVisitor());
+            System.out.println("Union Right: " + opUnion.getRight());
+            OpWalker.walk(opUnion.getRight(), new AuthOpBGPVisitor());
+
         }
     }
 
